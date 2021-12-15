e573e72830c3d11a3772b1616be26832f147e90b
m4rsm4ps <79999161+m4rsm4ps@users.noreply.github.com>
2021-12-16 01:35:55 +0200

    polls app created

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!wx3d2mn5@p7(iaeg&8rcf&m13njjba(la^d73ng*6m*6+5man'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-!wx3d2mn5@p7(iaeg&8rcf&m13njjba(la^d73ng*6m*6+5man')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
